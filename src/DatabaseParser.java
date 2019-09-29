4d4cb992a73d6413b9abb74ba3e50cfef7939d6d
Caleb Herring <cherring1@harding.edu>
2019-09-29 19:22:38 -0400

    Update DatabaseParser.java

--- src/DatabaseParser.java
+++ src/DatabaseParser.java
@@ -4,11 +4,13 @@
 import java.util.*;
 public class DatabaseParser {
 
+	private Vector<Vector<String>> database = new Vector<Vector<String>>();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Create 2D vector
-		Vector<Vector<String>> database = new Vector<Vector<String>>();
+		
 		
 		//Call read from file function that will enter data into vector
 		//call matrixScan to sort through good/bad data
