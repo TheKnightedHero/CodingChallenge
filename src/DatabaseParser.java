cb10632621b9fa0c98aeb0048e1aa7c722ac84bb
Caleb Herring <cherring1@harding.edu>
2019-09-29 19:12:41 -0400

    create 2D vector called database

--- src/DatabaseParser.java
+++ src/DatabaseParser.java
@@ -1,11 +1,26 @@
 /*
  	Author: Caleb Herring
 */
+import java.util.*;
 public class DatabaseParser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Create 2D vector
+		Vector<Vector<String>> database = new Vector<Vector<String>>();
+		
+		//Call read from file function that will enter data into vector
+		//call matrixScan to sort through good/bad data
+			//This function should also count:
+				//# of records received
+				//# of bad entries
+				//# of successful entries
+		//call function to write 
+	}
+	
+	public void readFromFile() {
+		
 	}
 
 }
