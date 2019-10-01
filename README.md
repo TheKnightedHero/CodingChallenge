de639eb0e56421660b4dd1dcaa33c36083f40d1b
Caleb Herring <cherring1@harding.edu>
2019-09-30 22:53:50 -0400

    added good .csv file and updated readMe

--- README.md
+++ README.md
@@ -7,18 +7,21 @@ Simply load the porject file and run the program through the IDE.
 I used Eclipse as my IDE.
 You could probably run it through the command prompt, I just never have before.
 
+Make sure the .jar files are added in propetires when loaded into the IDE. It should be already applied, but just in case. It will not function correctly without them.
+
 The application should: 
 
 Take the given .csv file renamed codingChallengeRecords.csv
 parse the data
 sort the data between Valid entries and Invalid entires.
 Write the bad data into a new .csv file
+Write the good data into a new .csv file, posing as the "database"
 write a log.txt file listing how many records were recorded, how many were Valid, and how many were Invalid
 
 -------------------
 # Design Choice
 
-A lot of my design choices were based on my limited knowledge in Java, having not touched the language in over 2-3 years and only recently using this weekend as a crash-course refresher. I also have never made a database before with Java, so i based the application on simply reading from the provided .csv file.
+A lot of my design choices arose because I haven't developed in Java in about 2-3 years and have used this weekend as a crash-course refresher. I also have never made a database before with Java, instead I decided to output the good data into a new .csv file.
 I did, however, try to keep to good coding practices as best as I could, such as having multiple functions carrying out different tasks.
 
 I approached the project under the assumption that blank fields were acceptable, and just acting as indicators of missing data entires. This allowed me to focus on making sure the data given met the requirement that data outside the 10 column limit was considered bad data.
@@ -26,7 +29,8 @@ I approached the project under the assumption that blank fields were acceptable,
 I split the project into three objectives, which are shown through the three functions I wrote: 
 1) Read from the .csv and store bad data
 2) Write bad data to a new.csv file
-3) Write a log.txt file
+3) Write good data to a new .csv file
+4) Write a log.txt file
 
 I grabbed a few custom Java libraries to handle the escaped characters thanks to the image data that was included in the .csv file. This allowed me to have a very simple and strightfoward setup, without writing a custom parser that could potentially miss entries and read the data as is.
 
--- src/DatabaseParser.java
+++ src/DatabaseParser.java
@@ -11,8 +11,8 @@ import com.opencsv.CSVWriter;
 public class DatabaseParser {
 
 	private List<List<String>> badRecords = new ArrayList<>();
+	private List<List<String>> goodRecords = new ArrayList<>();
 	private File csvFile = new File("./src/codingChallengeRecords.csv");
-	//private String csvSplitBy = ",";
 	private int numOfFailedRecords = 0;
 	private int numOfGoodRecords = 0;
 	private int numOfRecords = 0;
@@ -21,17 +21,15 @@ public class DatabaseParser {
 	public void readFromFile() 
 	{
 		try {
-			//BufferedReader br = new BufferedReader(new FileReader(csvFile));
 			CSVReader reader = new CSVReader(new FileReader(csvFile));
 			String[] lineInput = reader.readNext();
 			
 			while(lineInput != null) 
 			{
 				//increment the number of records we have received
-				numOfRecords++;
-				//String[] inputValues = lineInput.split(csvSplitBy);			
+				numOfRecords++;			
 
-				//check to see if number of columns match or if there is an empty column column
+				//check to see if number of columns match
 				//add to badRecords array if true
 				if(lineInput.length > 10) 
 				{
@@ -41,6 +39,7 @@ public class DatabaseParser {
 				else
 				{
 					numOfGoodRecords++;
+					goodRecords.add(Arrays.asList(lineInput));
 				}
 				lineInput = reader.readNext();
 			}
@@ -50,6 +49,7 @@ public class DatabaseParser {
 			numOfRecords--;
 			
 			reader.close();
+			
 		} catch(Exception e) {
 			System.out.println("Something happened when reading the CSV");
 		}
@@ -61,16 +61,12 @@ public class DatabaseParser {
 		{
 			CSVWriter csvWriter = new CSVWriter(new FileWriter("badEntries.csv"));
 			
+			//Add column headers to bad.csv file
 			String[] header = ("A,B,C,D,E,F,G,H,I,J").split(",");
 			csvWriter.writeNext(header);
-			//csvWrite.append("A,B,C,D,E,F,G,H,I,J");
-			//csvWriter.append("\n");
 			
 			for(List<String> rowData:badRecords)
 			{
-				//csvWriter.append(String.join(csvSplitBy, rowData));
-				//csvWriter.append("\n");
-				
 				csvWriter.writeNext((String[]) rowData.toArray());
 			}
 			
@@ -79,6 +75,25 @@ public class DatabaseParser {
 			
 		} catch(Exception e) {
 			System.out.println("Failed to write to badEntries.csv");
+		}
+	}
+	
+	public void writeGoodCsvToFile() 
+	{
+		try 
+		{
+			CSVWriter csvWriter = new CSVWriter(new FileWriter("goodEntries.csv"));
+			
+			for(List<String> rowData:goodRecords)
+			{
+				csvWriter.writeNext((String[]) rowData.toArray());
+			}
+			
+			csvWriter.flush();
+			csvWriter.close();
+			
+		} catch(Exception e) {
+			System.out.println("Failed to write to goodEntries.csv");
 		}
 	}
 	
@@ -105,6 +120,7 @@ public class DatabaseParser {
 		DatabaseParser Data = new DatabaseParser();
 		
 		Data.readFromFile();
+		Data.writeGoodCsvToFile();
 		Data.writeBadCsvToFile();
 		Data.writeLogToFile();
 	}
