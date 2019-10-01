c81f0c07f5b7eb39f50f1b300f010d1c1e5daac5
Caleb Herring <cherring1@harding.edu>
2019-09-30 21:52:21 -0400

    add libraries and finish project

--- src/DatabaseParser.java
+++ src/DatabaseParser.java
@@ -5,11 +5,14 @@
 import java.io.*;
 import java.util.*;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 public class DatabaseParser {
 
 	private List<List<String>> badRecords = new ArrayList<>();
 	private File csvFile = new File("./src/codingChallengeRecords.csv");
-	private String csvSplitBy = ",";
+	//private String csvSplitBy = ",";
 	private int numOfFailedRecords = 0;
 	private int numOfGoodRecords = 0;
 	private int numOfRecords = 0;
@@ -18,29 +21,35 @@ public class DatabaseParser {
 	public void readFromFile() 
 	{
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(csvFile));
-			String lineInput = br.readLine();
+			//BufferedReader br = new BufferedReader(new FileReader(csvFile));
+			CSVReader reader = new CSVReader(new FileReader(csvFile));
+			String[] lineInput = reader.readNext();
 			
 			while(lineInput != null) 
 			{
 				//increment the number of records we have received
 				numOfRecords++;
-				String[] inputValues = lineInput.split(csvSplitBy);
+				//String[] inputValues = lineInput.split(csvSplitBy);			
 
-				//check to see if number of columns match or if there is a missing column
+				//check to see if number of columns match or if there is an empty column column
 				//add to badRecords array if true
-				if(inputValues.length > 10) 
+				if(lineInput.length > 10) 
 				{
 					numOfFailedRecords++;
-					badRecords.add(Arrays.asList(inputValues));
+					badRecords.add(Arrays.asList(lineInput));
 				}
 				else
 				{
 					numOfGoodRecords++;
 				}
+				lineInput = reader.readNext();
 			}
 			
-			br.close();
+			//subtract 1 from numOfRecords and numOfGoodRecords since initial pass is reading in the column names
+			numOfGoodRecords--;
+			numOfRecords--;
+			
+			reader.close();
 		} catch(Exception e) {
 			System.out.println("Something happened when reading the CSV");
 		}
@@ -50,15 +59,19 @@ public class DatabaseParser {
 	{
 		try 
 		{
-			FileWriter csvWriter = new FileWriter("badEntries.csv");
+			CSVWriter csvWriter = new CSVWriter(new FileWriter("badEntries.csv"));
 			
-			csvWriter.append("A,B,C,D,E,F,G,H,I,J");
-			csvWriter.append("\n");
+			String[] header = ("A,B,C,D,E,F,G,H,I,J").split(",");
+			csvWriter.writeNext(header);
+			//csvWrite.append("A,B,C,D,E,F,G,H,I,J");
+			//csvWriter.append("\n");
 			
 			for(List<String> rowData:badRecords)
 			{
-				csvWriter.append(String.join(csvSplitBy, rowData));
-				csvWriter.append("\n");
+				//csvWriter.append(String.join(csvSplitBy, rowData));
+				//csvWriter.append("\n");
+				
+				csvWriter.writeNext((String[]) rowData.toArray());
 			}
 			
 			csvWriter.flush();
